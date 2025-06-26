459da92e618f71195857f688ffffebf14ffb5dd5
rozzy <rosekaremeri@gmail.com>
2025-06-26 18:15:12 +0300

    Feature: Wallet card

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,25 @@
 import ConnectWalletBTN from "@/components/ConnectWalletBTN";
+import WalletCard from "@/components/WalletCard";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="">
       <div className="flex justify-between p-2 pt-[1rem]">
-        <h1 className="text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 mb-2 sm:mb-0 drop-shadow-lg">Testnet Token Dashboard</h1>
+        <h1 className="text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 mb-2 sm:mb-0 drop-shadow-lg">
+          Testnet Token Dashboard
+        </h1>
         <ConnectWalletBTN />
+      </div>
+      <div className="grid grid-cols-2 gap-4 pt-10">
+
+
+
+       <WalletCard />
+
+
+
+        <div>choose a token to see history</div>
       </div>
     </div>
   );
---
+++ components/WalletCard.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import React from "react";
+
+const WalletCard = () => {
+  return (
+    <div className="p-4 ">
+      <div
+        className="relative bg-gradient-to-br from-[#57064f] via-[#7e2a77] to-[#a34f9a]
+                    p-6 rounded-xl shadow-lg text-white w-[80%] h-52 flex flex-col justify-between
+                    font-mono overflow-hidden"
+      >
+        <div className="flex flex-col items-start">
+          <p className="text-sm font-semibold tracking-wider opacity-90">
+            My Wallet
+          </p>
+          <h3 className="text-l font-bold tracking-widest break-all">
+            0x9837...0723
+          </h3>
+        </div>
+        <div className="flex justify-between items-end mt-4">
+          <div>
+            <p className="text-sm opacity-90">Balance</p>
+            <h1 className="text-4xl font-bold">USDT 0.009876</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WalletCard;
